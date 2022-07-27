Fix gradients, optimise model

src/run_sf.py
@@ -140,10 +140,16 @@ if __name__ == "__main__":
         loss = jnp.mean(optax.l2_loss(y_pred, y))
         return loss
 
-    jax.grad(compute_loss)(params, true_data)
+    # jax.grad(compute_loss)(params, true_data)
     jax.grad(compute_loss)(jnp.array([2.1, 1.0]), true_data)
 
-    # for _ in range(1000):
-    #     grads = jax.grad(compute_loss)(params, true_data)
-    #     updates, opt_state = optimizer.update(grads, opt_state)
-    #     params = optax.apply_updates(params, updates)
+    # Train:
+    for _ in range(200):
+        grads = jax.grad(compute_loss)(params, true_data)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+        # Printing the loss is very inefficient, speed up the training by removing this line
+        print(f"Parameters: {params}, Loss: {compute_loss(params, true_data)}")
+
+    # Final parameters:
+    print(f"Final parameters, {params}")

src/socialforce.py
@@ -5,22 +5,7 @@ from jax import random, lax
 from jax import vmap
 from jax.config import config
 
-
-def V(distance, strength, shape):
-    return strength * jnp.exp(-distance / shape)
-
-
-def VPrime(distance, strength, shape):
-    return -V(distance, strength, shape) / shape
-
-
-@jax.jit
-def pedestrian_repulsion(xi, xj, strength, shape):
-    Delta_x = xj - xi
-    distance = jnp.clip(jnp.linalg.norm(xj - xi), 0.1, jnp.inf)
-    v = VPrime(distance, strength, shape) / distance
-    return jnp.where(jnp.alltrue(Delta_x == 0), x=jnp.zeros(2), y=v * Delta_x)
-    # return lax.cond(jnp.alltrue(Delta_x == 0), lambda: v * Delta_x, lambda: jnp.zeros(2))
+# from utils import vectorized_cond
 
 
 def two_groups_datum(key, N, D=5, V=2):
@@ -45,11 +30,59 @@ def relax_to_desired(current_velocity, desired_velocity, relaxation_time):
 vrelax_to_desired = vmap(relax_to_desired, (0, 0, None))
 
 
+def V(distance, strength, shape):
+    return strength * jnp.exp(-distance / shape)
+
+
+def VPrime(distance, strength, shape):
+    return -V(distance, strength, shape) / shape
+
+
+@jax.jit
+def pedestrian_repulsion(xi, xj, strength, shape):
+    Delta_x = xj - xi
+    # jnp.clip(d, 0.1, 10.0)
+    distance = jnp.linalg.norm(Delta_x)
+    # distance = jnp.power(jnp.power(Delta_x[0], 2) + jnp.power(Delta_x[1], 2), 1 / 2)
+    # distance = 1.0
+    v = VPrime(distance, strength, shape) / (distance + 0.1)
+    # return lax.cond(
+    #     jnp.alltrue(Delta_x == 0), lambda x: v * x, lambda x: jnp.zeros(2), Delta_x
+    # )
+    return Delta_x * v
+    # return (distance, strength, shape)
+
+
+# def total_pedestrian_repulsion(xi, xs, strength, shape):
+#     forces = vmap(
+#         pedestrian_repulsion,
+#         (None, 0, None, None),
+#     )(xi, xs, strength, shape)
+#     return jnp.sum(forces, 0, where=jnp.invert(jnp.isnan(forces)))
+
+
+def select_different(point, points, x=None, y=None, size=None, fill_value=None):
+    return points[
+        jnp.where(
+            ~vmap(jnp.array_equal, (0, None))(points, point),
+            x=x,
+            y=y,
+            size=size,
+            fill_value=fill_value,
+        )
+    ]
+
+
 def total_pedestrian_repulsion(xi, xs, strength, shape):
+    xs_without_xi = select_different(
+        xi,
+        xs,
+        size=len(xs) - 1,
+    )
     forces = vmap(
         pedestrian_repulsion,
         (None, 0, None, None),
-    )(xi, xs, strength, shape)
+    )(xi, xs_without_xi, strength, shape)
     return jnp.sum(forces, 0, where=jnp.invert(jnp.isnan(forces)))
 
 
@@ -77,7 +110,16 @@ def get_closest_wall(xi, walls):
     return closest_points[jnp.argmin(distances), :]
 
 
+def in_segment(pt, seg, epsilon=1e-6):
+    a, b = pt - seg[0:2], pt - seg[2:4]
+    cp = jnp.cross(a, b)
+    dp = jnp.dot(a, b)
+    return abs(cp) <= epsilon and dp > 0 and jnp.linalg.norm(a, b) < dp
+
+
 def total_wall_repulsion(xi, walls, strength, shape):
+    # doesn't work because dynamic sized walls
+    # walls = walls[jnp.where(~vmap(in_segment, (None, 0))(xi, walls), size=len(walls))]
     xs = vmap(closest_point_to_vsegment, (None, 0))(xi, walls)
     forces = vmap(
         pedestrian_repulsion,

test/test_socialforce.py
@@ -9,15 +9,15 @@ from jax.config import config
 import src.socialforce as sf
 from src import transformations
 
-config.update("jax_debug_nans", True)
+config.update("jax_debug_nans", False)
 
 key = random.PRNGKey(42)
 
 
 def no_nan_gradients():
     N = 40
-    width = 5
-    height = 3
+    width = 5.0
+    height = 3.0
     x0 = sf.two_groups_datum(key, N, width, 0.66 * height)
     xs = x0[:, 0:2]
     pedestrian_strength = 2.1
@@ -34,14 +34,14 @@ def no_nan_gradients():
     current_vel = x0[0, 2:4]
     good_vel = current_vel + jnp.array([1.0, 1.0])  # as in shouldn't cause NaN
     bad_vel = current_vel
-    relax_good_current_grad, relax_good_target_grad, relax_good_time_grad = jax.jacfwd(
+    relax_good_current_grad, relax_good_target_grad, relax_good_time_grad = jax.jacrev(
         sf.relax_to_desired, (0, 1, 2)
     )(current_vel, good_vel, relaxation_time)
     assert ~jnp.all(jnp.isnan(relax_good_current_grad))
     assert ~jnp.all(jnp.isnan(relax_good_target_grad))
     assert ~jnp.all(jnp.isnan(relax_good_time_grad))
 
-    relax_bad_current_grad, relax_bad_target_grad, relax_bad_time_grad = jax.jacfwd(
+    relax_bad_current_grad, relax_bad_target_grad, relax_bad_time_grad = jax.jacrev(
         sf.relax_to_desired, (0, 1, 2)
     )(current_vel, bad_vel, relaxation_time)
     assert ~jnp.all(jnp.isnan(relax_bad_current_grad))
@@ -52,21 +52,31 @@ def no_nan_gradients():
 
     # Pedestrian repulsion gradients
     ped_ped_dist = 0.0
-    V_dist_grad, V_str_grad, V_shape_grad = jax.grad(sf.V, (0, 1, 2))(
+    V_dist_grad, V_str_grad, V_shape_grad = jax.jacrev(sf.V, (0, 1, 2))(
         ped_ped_dist, pedestrian_strength, pedestrian_shape
     )
     assert ~jnp.isnan(V_dist_grad)
     assert ~jnp.isnan(V_str_grad)
     assert ~jnp.isnan(V_shape_grad)
 
-    VPrime_dist_grad, VPrime_str_grad, VPrime_shape_grad = jax.grad(sf.V, (0, 1, 2))(
+    VPrime_dist_grad, VPrime_str_grad, VPrime_shape_grad = jax.jacrev(sf.V, (0, 1, 2))(
         0.1, pedestrian_strength, pedestrian_shape
     )
     assert ~jnp.isnan(VPrime_dist_grad)
     assert ~jnp.isnan(VPrime_str_grad)
     assert ~jnp.isnan(VPrime_shape_grad)
 
-    tpr_xi_grad, tpr_xs_grad, tpr_str_grad, tpr_shape_grad = jax.jacfwd(
+    # return jax.jacrev(sf.pedestrian_repulsion)(
+    #     xs[0], xs[0], pedestrian_strength, pedestrian_shape
+    # )
+
+    # return jax.jacrev(sf.pedestrian_repulsion)(
+    #     xs[0], xs[0], pedestrian_strength, pedestrian_shape
+    # )
+    # return sf.total_pedestrian_repulsion(
+    #     xs[0], xs, pedestrian_strength, pedestrian_shape
+    # )
+    tpr_xi_grad, tpr_xs_grad, tpr_str_grad, tpr_shape_grad = jax.jacrev(
         sf.total_pedestrian_repulsion, (0, 1, 2, 3)
     )(xs[0], xs, pedestrian_strength, pedestrian_shape)
 
@@ -75,7 +85,7 @@ def no_nan_gradients():
     assert ~jnp.all(jnp.isnan(tpr_str_grad))
     assert ~jnp.all(jnp.isnan(tpr_shape_grad))
 
-    ptpr_xs_grad, ptpr_str_grad, ptpr_shape_grad = jax.jacfwd(
+    ptpr_xs_grad, ptpr_str_grad, ptpr_shape_grad = jax.jacrev(
         sf.pairwise_total_pedestrian_repulsion, (0, 1, 2)
     )(xs, pedestrian_strength, pedestrian_shape)
 
@@ -84,17 +94,20 @@ def no_nan_gradients():
     assert ~jnp.all(jnp.isnan(ptpr_shape_grad))
 
     # Wall force grads
-    point = jnp.array([0.0, 0.0])
+    point = jnp.array([0.0, 2.9])  # if a point touches a segment, we get `nan`s
+    points = jnp.array([[0.0, 2.9], [1.0, 0.9]])
     segment = jnp.array([-1.0, 1.0, 2.0, 1.0])
-    vseg_point_grad, vseg_segment_grad = jax.jacfwd(
+    vseg_point_grad, vseg_segment_grad = jax.jacrev(
         sf.closest_point_to_vsegment, (0, 1)
     )(point, segment)
 
     assert ~jnp.all(jnp.isnan(vseg_point_grad))
     assert ~jnp.all(jnp.isnan(vseg_segment_grad))
-
-    # total_wall_repulsion,
-    # pairwise_total_wall_repulsion,
+    # return (point, walls, wall_strength, wall_shape)
+    # return jax.jacrev(sf.total_wall_repulsion)(point, walls, wall_strength, wall_shape)
+    # return jax.jacrev(sf.pairwise_total_wall_repulsion)(
+    #     points, walls, wall_strength, wall_shape
+    # )
 
 
 no_nan_gradients()